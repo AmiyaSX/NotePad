cc927c5c9424c85cecb485364daa7f8dba8f17ee
sunxi <sunxi@highlight.mobi>
2022-10-04 13:26:24 +0800

    Initial Commit

---
+++ NotePad/NotePadApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotePadApp.swift
+//  NotePad
+//
+//  Created by rockey220505 on 2022/10/4.
+//
+
+import SwiftUI
+
+@main
+struct NotePadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
